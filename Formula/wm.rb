f61b52576c6b639c218e694bbd9dbc88dc0e94b9
Muness Castle <931+muness@users.noreply.github.com>
2026-01-15 21:40:18 -0500

    Update Homebrew formula for v0.3.2

--- Formula/wm.rb
+++ Formula/wm.rb
@@ -1,8 +1,8 @@
 class Wm < Formula
   desc "Working memory for AI coding assistants"
   homepage "https://github.com/cloud-atlas-ai/wm"
-  url "https://github.com/open-horizon-labs/wm/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "4464692a0f30c23da04f4fb006f2ce9de28a414548efbfe833ca60f68b30605e"
+  url "https://github.com/open-horizon-labs/wm/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "8ebd3a36953817459ae17f639aa38c5c3de58a6bd4b77502c048b639432bb6d4"
   license :cannot_represent
 
   depends_on "rust" => :build
